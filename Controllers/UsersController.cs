e943b4fe20f88b20fde66f5d44ba5ae4a2aeb85d
TomaszSteblik <tsteblik@gmail.com>
2020-12-02 17:57:48 +0100

    Dodanie generowania jwt podczas logowaia

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,8 +1,15 @@
+using System;
+using System.Collections.Generic;
 using api.Data;
 using api.DTOs;
 using api.Models;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
 
 namespace api.Controllers
 {
@@ -27,12 +34,33 @@ namespace api.Controllers
             if (userItem == null) return NotFound();
             return Ok(_mapper.Map<UserReadDto>(userItem));
         }
-        [HttpGet("login/{id}")]
-        public ActionResult<UserReadDto> GetUserByLoginId(string id)
+        [HttpPost("login")]
+        public ActionResult<Object> GetUserByLoginId(string googleId)
         {
-            var userItem = _repo.GetUserByLoginId(id);
+            
+            var userItem = _repo.GetUserByLoginId(googleId);
             if (userItem == null) return NotFound();
-            return Ok(_mapper.Map<UserReadDto>(userItem));
+            
+            string key = "dlkfjg0934u5tdg54g";
+            var issuer = "podzielsieksiazka.northeurope.cloudapp.azure.com";
+            
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha256);
+            
+            var permClaims = new List<Claim>();
+            permClaims.Add(new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()));
+            permClaims.Add(new Claim("valid","1"));
+            permClaims.Add(new Claim("googleId",googleId));
+            permClaims.Add(new Claim("id","1"));
+            
+            var token = new JwtSecurityToken(issuer,
+                issuer, 
+                permClaims,    
+                expires: DateTime.Now.AddDays(1),    
+                signingCredentials: credentials);    
+            var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);    
+            return new { token = jwtToken, user = _mapper.Map<UserReadDto>(userItem)};   
+            
             
         }
 
